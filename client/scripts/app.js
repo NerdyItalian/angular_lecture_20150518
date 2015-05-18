6b96987c35fb30c6264d08c153926894634b8c46
NerdyItalian <atava8384@gmail.com>
2015-05-18 13:32:56 -0500

    initial commit

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,15 @@
-console.log("Script Loaded");
+//This is how we declare a new module for angular.
+// angular is VERY important -- its tell angular that we area talking to it.
+// the 'myApp' in the parameters so that it knows when we use that variable that we are doing this call.
+// the empty brackets signify grabbing the dependencies we need
+// controller is additional functionality that we want to add.
+var myApp = angular.module('myApp',[] ); // angular.module is export this module to be used in the HTML
+
+//the ng-controller in the html is calling this .controller by using the "WelcomeController"
+myApp.controller("WelcomeController", ["$scope", function($scope){
+    $scope.heading = "Message: ";
+
+    $scope.updateMessage = function(){
+        $scope.message = "Hello " + $scope.name + "!";
+    };
+}]);
